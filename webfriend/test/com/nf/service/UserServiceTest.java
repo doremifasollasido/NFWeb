72f544f4725b2c7bb587e86b6838048e1f82a85d
xianiaoxiaoxuesheng <420923610@qq.com>
2014-09-19 14:46:52 +0800

    11111

--- webfriend/test/com/nf/service/UserServiceTest.java
+++ webfriend/test/com/nf/service/UserServiceTest.java
@@ -19,13 +19,13 @@ import com.nf.model.User;
 
 public class UserServiceTest
 {
-	private  UserService userService;
+	private  UserService userservice;
 	@Before
 	public  void setUpBeforeClass(){
 		System.out.println("加载配置文件……");
 		ApplicationContext context = new FileSystemXmlApplicationContext(new String[]{"src/beans.xml"});
 		System.out.println("加载配置文件成功");
-		userService = (UserService) context.getBean("userService");
+		userservice = (UserService) context.getBean("userservice");
 	}
 	@Test
 	public void testExists()
@@ -34,9 +34,28 @@ public class UserServiceTest
 	}
 
 	@Test
-	public void testAdd()
+	public void testAddUser() throws Exception
 	{
-		
+		User user = new User();
+		for(int i=0 ;i<10;i++)
+		{
+		user.setAlais("煞笔啊啊"+i);
+		user.setAt_state(i);
+		user.setClassnumber(i+2);
+		user.setAddress("广州"+i);
+		user.setId(10+i);
+		user.setPassword("123456"+i);
+		user.setUsername("毒逍遥"+i);
+		user.setDept("123456"+i);
+		user.setMajor("计算机"+i);
+		user.setInfo_state(i);
+		user.setSno("123456+i");
+		user.setEmail(i+"123456@qq.com");
+		user.setImage("111"+i);
+		ApplicationContext context = new FileSystemXmlApplicationContext(new String[]{"src/beans.xml"});
+		UserService userservice= (UserService)context.getBean("userservice");
+		userservice.save(user);
+		}
 	}
 
 
